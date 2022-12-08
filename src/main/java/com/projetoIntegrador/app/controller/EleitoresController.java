01dca74758a0e599e9e1e0976817da57aa88baf4
Marcelo Klosouski Junior <klosouski96@gmail.com>
2022-12-08 13:44:03 -0300

    add eleitores controller

--- src/main/java/com/projetoIntegrador/app/controller/EleitoresController.java
+++ src/main/java/com/projetoIntegrador/app/controller/EleitoresController.java
@@ -1,25 +1,25 @@
-//package com.projetoIntegrador.app.controller;
-//
-//import com.projetoIntegrador.app.model.auth.Eleitores;
-//import com.projetoIntegrador.app.repository.EleitoresRepository;
-//import java.util.List;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RequestMapping("/v1/eleitores")
-//@RestController
-//public class EleitoresController {
-//
-//  private final EleitoresRepository repository;
-//
-//  public EleitoresController(EleitoresRepository repository) {
-//    this.repository = repository;
-//  }
-//
-//  @GetMapping("/listar")
-//  public List<Eleitores> getAll(){
-//    return repository.findAll();
-//  }
-//
-//}
+package com.projetoIntegrador.app.controller;
+
+import com.projetoIntegrador.app.model.auth.Eleitores;
+import com.projetoIntegrador.app.repository.EleitoresRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/v1/eleitores")
+@RestController
+public class EleitoresController {
+
+  private final EleitoresRepository repository;
+
+  public EleitoresController(EleitoresRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping("/listar")
+  public List<Eleitores> getAll(){
+    return repository.findAll();
+  }
+
+}
--- src/main/java/com/projetoIntegrador/app/repository/EleitoresRepository.java
+++ src/main/java/com/projetoIntegrador/app/repository/EleitoresRepository.java
@@ -1,10 +1,10 @@
-//package com.projetoIntegrador.app.repository;
-//
-//import com.projetoIntegrador.app.model.auth.Eleitores;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface EleitoresRepository extends JpaRepository<Eleitores, Long> {
-//
-//}
+package com.projetoIntegrador.app.repository;
+
+import com.projetoIntegrador.app.model.auth.Eleitores;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EleitoresRepository extends JpaRepository<Eleitores, Long> {
+
+}
